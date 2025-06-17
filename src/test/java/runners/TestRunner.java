a9c3c62a04af2275290fb928e4f81fc1cbbc4376
LathaSm <41245939+LathaSm@users.noreply.github.com>
2025-06-17 17:50:13 -0500

    Updated changes with POM.xml

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -8,10 +8,14 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    features = "src/test/resources/features",
+    features = "src/test/resources/features/Login.feature",
     glue = "stepdefinitions",
-    plugin = {"pretty", "html:target/cucumber-reports"
-    		},
+       		plugin = {
+    				  "pretty",
+    				  "json:target/cucumber-reports/Cucumber.json",
+    				  "html:target/cucumber-reports"
+    				},
+
     tags = "not @skip"
 )
 public class TestRunner {
